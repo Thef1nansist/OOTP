94d8744042026f0fad2b72b0db21b25f322b7768
evgeniy <vkzheny@gmail.com>
2020-10-09 01:08:08 +0300

    na poebis'

--- OOP_4Lab/List.cs
+++ OOP_4Lab/List.cs
@@ -1,51 +1,184 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Runtime.CompilerServices;
-using System.Text;
 
-namespace OOP_4Lab
+namespace SimpleAlgorithmsApp
 {
-    public class List<T>
+    public class LinkedList<T> : IEnumerable<T>  // односвязный список
     {
-        public List(T data)
-        {
-            Data = data;
-        }
-        public List()
-        {
-             // здесь ошибка точно
-        }
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
 
-        public T Data { get; set; }
-        public List<T> Next { get; set; }
-
-        List<T> head;
-        List<T> tail;
-        int countofSymbols;
-
-        public void Add(T date)
+        // добавление элемента
+        public void Add(T data)
         {
-            List<T> list = new List<T>(date);
+            Node<T> node = new Node<T>(data);
 
             if (head == null)
-                head = list;
+                head = node;
             else
-                tail.Next = list;
-            tail = list;
+                tail.Next = node;
+            tail = node;
 
-            countofSymbols++;
+            count++;
         }
-        public static List<T> operator >>(List<T> op1, List<T> op2) // давай
-        {   
-            return new List<T>();
+        // удаление элемента
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
         }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+        // очистка списка
         public void Clear()
         {
             head = null;
             tail = null;
-            countofSymbols = 0;
+            count = 0;
+        }
+        // содержит ли список элемент
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        // добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
         }
 
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public static LinkedList<T> operator >>(LinkedList<T> list, int index)
+        {
+
+            Node<T> current = list.head;
+            Node<T> previous = null;
+            var i = 0;
+
+            while (current != null)
+            {
+                if (i == index)
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            list.tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        list.head = list.head.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (list.head == null)
+                            list.tail = null;
+                    }
+                    list.count--;
+                }
+                i++;
+                previous = current;
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public static bool operator +(LinkedList<T> list, int index)
+        {
+            return false;
+        }
+
+        public static bool operator !=(LinkedList<T> list, LinkedList<T> list2)
+        {
+            return !list.Equals(list2);
+        }
+
+        public static bool operator ==(LinkedList<T> list, LinkedList<T> list2)
+        {
+            return list.Equals(list2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return false;
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
     }
 }
