162a65f42ca0b40ff7ee604787e5543b7bcc3693
Thef1nansist <vladsavhchenko994@gmail.com>
2020-10-08 23:48:30 +0300

    Ds

---
+++ OOP_4Lab/List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OOP_4Lab
+{
+    public class List<T>
+    {
+        public List(T data)
+        {
+            Data = data;
+        }
+        public List()
+        {
+            
+        }
+
+        public T Data { get; set; }
+        public List<T> Next { get; set; }
+
+        List<T> head;
+        List<T> tail;
+        int countofSymbols;
+
+        public void Add(T date)
+        {
+            List<T> list = new List<T>(date);
+
+            if (head == null)
+                head = list;
+            else
+                tail.Next = list;
+            tail = list;
+
+            countofSymbols++;
+        }
+        public static List<T> operator <<(List<T> op1, List<T> op2)
+        {
+            return new List<T>(); и что делать? o
+        }
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            countofSymbols = 0;
+        }
+
+    }
+}
---
+++ OOP_4Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_4Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
